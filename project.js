6a039022dafe5c5caec1b3d862546f998d86a63b
Jacob Garcia <jgarcia224@mail.csuchico.edu>
2017-03-31 16:26:00 -0700

    almost there just need to figure out the onsight validate and maybe the cookies

--- project.js
+++ project.js
@@ -5,19 +5,39 @@ var userEntered = document.getElementById("user3").value;
 
 
 
-var {}
-
-function{}
-
-if (userentered.value)==7 {
+if (userentered.value)==X
+{
   document.getElementById("usernameError").innerHTML="thats corect you rascle.";
   document.getElementById("usernameError").classList.remove("hidden-message");
   document.getElementById("usernameError").classList.add("shown-message");
-
 }
-else {
+else
+{
   document.getElementById("usernameError").innerHTML="incorect value.";
   document.getElementById("usernameError").classList.remove("hidden-message");
   document.getElementById("usernameError").classList.add("shown-message");
 }
+if (userentered.value)==Y
+{
+  document.getElementById("usernameError").innerHTML="thats corect you rascle.";
+  document.getElementById("usernameError").classList.remove("hidden-message");
+  document.getElementById("usernameError").classList.add("shown-message");
 }
+else
+{
+  document.getElementById("usernameError").innerHTML="incorect value.";
+  document.getElementById("usernameError").classList.remove("hidden-message");
+  document.getElementById("usernameError").classList.add("shown-message");
+}
+if (userentered.value)==Z {
+  document.getElementById("usernameError").innerHTML="thats corect you rascle.";
+  document.getElementById("usernameError").classList.remove("hidden-message");
+  document.getElementById("usernameError").classList.add("shown-message");
+
+} else {
+
+}
+}
+var X=418456
+var Y=51984
+var Z=-1434240
