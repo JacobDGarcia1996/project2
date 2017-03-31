d798e4be96d3236ee3534940110dbf3a3228d6dc
Jacob Garcia <jgarcia224@mail.csuchico.edu>
2017-03-31 16:54:51 -0700

    still need to get on input runing

--- project.js
+++ project.js
@@ -1,11 +1,14 @@
 function validate(){
-var userEntered = document.getElementById("user1").value;
-var userEntered = document.getElementById("user2").value;
-var userEntered = document.getElementById("user3").value;
+  var X=418456
+  var Y=51984
+  var Z=-1434240
+var X =userEntered = document.getElementById("user1").value;
+var Y =userEntered = document.getElementById("user2").value;
+var Z =userEntered = document.getElementById("user3").value;
 
 
 
-if (userentered.value)==X
+if (userentered.value)=X
 {
   document.getElementById("usernameError").innerHTML="thats corect you rascle.";
   document.getElementById("usernameError").classList.remove("hidden-message");
@@ -38,6 +41,3 @@ if (userentered.value)==Z {
 
 }
 }
-var X=418456
-var Y=51984
-var Z=-1434240
