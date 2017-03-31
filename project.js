a88b8abf404d4d79fdfa48e84be1cb450168e2f9
Jacob Garcia <jgarcia224@mail.csuchico.edu>
2017-03-31 13:07:13 -0700

    still need to figure out my puzzles and corectly use the validation

---
+++ project.js
@@ -0,0 +1,23 @@
+function validate(){
+var userEntered = document.getElementById("user1").value;
+var userEntered = document.getElementById("user2").value;
+var userEntered = document.getElementById("user3").value;
+
+
+
+var {}
+
+function{}
+
+if (userentered.value)==7 {
+  document.getElementById("usernameError").innerHTML="thats corect you rascle.";
+  document.getElementById("usernameError").classList.remove("hidden-message");
+  document.getElementById("usernameError").classList.add("shown-message");
+
+}
+else {
+  document.getElementById("usernameError").innerHTML="incorect value.";
+  document.getElementById("usernameError").classList.remove("hidden-message");
+  document.getElementById("usernameError").classList.add("shown-message");
+}
+}
