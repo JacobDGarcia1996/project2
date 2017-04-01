a85b5ec0292e826c8feeab56b1830ea35585324b
Jacob Garcia <jgarcia224@mail.csuchico.edu>
2017-03-31 20:04:46 -0700

    still in trouble

--- project.js
+++ project.js
@@ -2,13 +2,9 @@ function validate(){
   var X=418456
   var Y=51984
   var Z=-1434240
-var X =userEntered = document.getElementById("user1").value;
-var Y =userEntered = document.getElementById("user2").value;
-var Z =userEntered = document.getElementById("user3").value;
+  var =userEntered = document.getElementById("user1").oninput ="function{}"
 
-
-
-if (userentered.value)=X
+if (userentered.value)=418456
 {
   document.getElementById("usernameError").innerHTML="thats corect you rascle.";
   document.getElementById("usernameError").classList.remove("hidden-message");
@@ -20,6 +16,12 @@ else
   document.getElementById("usernameError").classList.remove("hidden-message");
   document.getElementById("usernameError").classList.add("shown-message");
 }
+ }
+var Y =userEntered = document.getElementById("user2").oninput ="function";
+var Z =userEntered = document.getElementById("user3").oninput ="function";
+
+
+
 if (userentered.value)==Y
 {
   document.getElementById("usernameError").innerHTML="thats corect you rascle.";
